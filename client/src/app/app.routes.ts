ffe51fc7748ba3d6f10379c27e3b88f07947556c
varga madalina <madalina.g.varga@gmail.com>
2023-12-09 20:41:06 +0200

    redirect to login if no token

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,11 +1,15 @@
 import { Routes } from '@angular/router';
 import { EventComponent } from './eveniment/event/event.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 export const routes: Routes = [{
     path: 'auth', loadChildren: () => import("./auth/auth.module").then(m => m.AuthModule)
 },
 {
     path: 'event', component: EventComponent
+},
+{
+    path: 'dashboard', component: DashboardComponent
 },
 {
     path: '**', redirectTo: 'auth/login'
--- client/src/app/common/services/base.service.ts
+++ client/src/app/common/services/base.service.ts
@@ -1,19 +1,34 @@
 import { HttpHandlerFn, HttpInterceptorFn, HttpRequest } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { catchError, throwError } from 'rxjs';
 
 export const BaseService: HttpInterceptorFn = (req: HttpRequest<unknown>,
   next: HttpHandlerFn,) => {
 
   const token = localStorage.getItem("token=");
   if (token) {
-    console.log("Am token: ", token)
-
     const clonedRequest = req.clone({
       setHeaders: {
         Authorization: `Bearer ${token}`
       }
     });
-    return next(clonedRequest);
+    return next(clonedRequest).pipe(
+      catchError((error) => {
+        if (error.status === 401) {
+          window.location.href = 'auth/login';
+        }
+        return throwError(error);
+      })
+    );
   } else {
-    return next(req);
+    return next(req).pipe(
+
+      catchError((error) => {
+        if (error.status === 401) {
+          window.location.href = 'auth/login';
+        }
+        return throwError(error);
+      })
+    );
   }
 };
